88a9364a0c8d65cb3b6227cb104eed5b837f6d48
Vijji11 <24b01a4511@svecw.edu.in>
2026-07-30 21:11:13 +0530

    Delete sum.ts

--- sum.ts
+++
@@ -1,9 +0,0 @@
-function sum(...numbers: number[]): number {
-    let total = 0;
-    for (let num of numbers) {
-        total += num;
-    }
-    return total;
-}
-
-console.log(sum(1, 2, 3, 4, 5));
