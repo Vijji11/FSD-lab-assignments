5b98f3c8548a9e9004c84ef03695646942f19379
Vijji11 <24b01a4511@svecw.edu.in>
2026-07-30 21:10:36 +0530

    Delete Calculator.js

--- Calculator.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-let A = 20;
-let B = 10;
-console.log("Addition:", A + B);
-console.log("Subtraction:", A - B);
-console.log("Multiplication:", A * B);
-console.log("Division:", A / B);
